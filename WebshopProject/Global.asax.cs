8a1f30e0a6093112325f8da7e698c7b82c6a0932
PFinnfors <pier@live.se>
2017-04-24 15:05:54 +0200

    Some script bundling and changed to local dependencies

--- WebshopProject/Global.asax.cs
+++ WebshopProject/Global.asax.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using WebshopProject.App_Start;
 
 namespace WebshopProject
 {
@@ -13,6 +11,7 @@ namespace WebshopProject
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
