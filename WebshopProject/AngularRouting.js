19946e73ee1d9d8a096128a0c6d721c3831b8bab
PFinnfors <pier@live.se>
2017-04-24 12:08:25 +0200

    Edited Angular code style to attempt a cross-computer fix

--- WebshopProject/AngularRouting.js
+++ WebshopProject/AngularRouting.js
@@ -1,76 +1,79 @@
 ﻿(function () {
     'use strict';
 
-    angular.module("shopService", []).
-        factory("ShopApi", function ($http) {
-            var urlBase = "http://localhost:49217/";
-            var ShopApi = {};
+    var shopServ = angular.module("shopService", []);
 
-            ShopApi.getCart = function () {
-                return $http.get(urlBase + "/Cart");
-            };
+    shopServ.factory("ShopApi", function ($http) {
+        var urlBase = "http://localhost:49217/api";
+        var ShopApi = {};
 
-            ShopApi.addToCart = function (movie) {
-                return $http.post(urlBase + "/Cart/", movie);
-            };
+        ShopApi.getCart = function () {
+            return $http.get(urlBase + "/Cart");
+        };
 
-            return ShopApi;
-        });
+        ShopApi.addToCart = function (movie) {
+            return $http.post(urlBase + "/Cart/", movie);
+        };
 
-    angular.module("shopApp", ["ngRoute", "shopService"]).
-        config(function ($routeProvider) {
-            $routeProvider
-                .when("/", {
-                    templateUrl: "/Products/Start"
-                })
-                .when("/JL", {
-                    templateUrl: "/Products/JL"
-                })
-                .when("/2", {
-                    templateUrl: "/Products/Product2"
-                })
-                .when("/top-2017", {
-                    templateUrl: "/Products/Top2017"
+        return ShopApi;
+    });
+
+    var shopA = angular.module("shopApp", ["ngRoute", "shopService"]);
+
+    shopA.config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "/Products/Start"
+            })
+            .when("/JL", {
+                templateUrl: "/Products/JL"
+            })
+            .when("/2", {
+                templateUrl: "/Products/Product2"
+            })
+            .when("/top-2017", {
+                templateUrl: "/Products/Top2017"
+            });
+    });
+
+    shopA.controller("IndexCtrl", function ($scope, ShopApi) {
+
+        $scope.message = "Welcome to the Main Controller!";
+
+        getCart();
+
+        function getCart() {
+            ShopApi.getCart().success(function (movies) {
+                $scope.movies = movies;
+            }).
+                error(function () {
+                    $scope.status = "Unable to load movies data!";
                 });
-        }).
-        controller("IndexCtrl", function ($scope, ShopApi) {
+        }
+    });
+    //shopA.controller("AddCtrl", function ($scope, ShopApi) {
 
-            $scope.message = "Welcome to the Main Controller!";
+    //    $scope.message = "Welcome from Add Controller!";
 
-            getCart();
+    //    $scope.addToCart = function () {
+    //        var addToMovie = {
+    //            //"Name": $scope.name,
+    //            //"Age": $scope.age,
+    //            //"Salary": $scope.sal
+    //        };
 
-            function getCart() {
-                ShopApi.getCart().success(function (movies) {
-                    $scope.movies = movies;
-                }).
-                    error(function () {
-                        $scope.status = "Unable to load movies data!";
-                    });
-            }
-        });
-        //controller("AddCtrl", function ($scope, ShopApi) {
+    //        ShopApi.addToCart(addToMovie).
+    //            success(function (response) {
+    //                alert("Movie added.");
+    //                //$scope.name = undefined;
+    //                //$scope.age = undefined;
+    //                //$scope.sal = undefined;
+    //            }).
+    //            error(function (response) {
+    //                alert("Error in adding movie!");
+    //            });
+    //    };
 
-        //    $scope.message = "Welcome from Add Controller!";
+    //});
 
-        //    $scope.addToCart = function () {
-        //        var addToMovie = {
-        //            //"Name": $scope.name,
-        //            //"Age": $scope.age,
-        //            //"Salary": $scope.sal
-        //        };
-
-        //        ShopApi.addToCart(addToMovie).
-        //            success(function (response) {
-        //                alert("Movie added.");
-        //                //$scope.name = undefined;
-        //                //$scope.age = undefined;
-        //                //$scope.sal = undefined;
-        //            }).
-        //            error(function (response) {
-        //                alert("Error in adding movie!");
-        //            });
-        //    };
-
-        //});
-    
 })();
